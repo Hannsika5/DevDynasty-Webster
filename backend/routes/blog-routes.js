d24421e971c91d9ea12e99b80483b7b88ad7bcab
Muskan-Yadav-369 <148032461+Muskan-Yadav-369@users.noreply.github.com>
2023-11-12 12:20:05 +0530

    Update blog-routes.js

--- backend/routes/blog-routes.js
+++ backend/routes/blog-routes.js
@@ -1,7 +1,10 @@
 import express from 'express';
-import {getAllBlogs} from "../controllers/Blog-controller.js";
+import {getById,updateBlog, addBlog,getAllBlogs } from '../controllers/blog-controller.js';
+
 const blogRouter=express.Router();
 
 blogRouter.get("/",getAllBlogs);
-
+blogRouter.post("/add",addBlog);
+blogRouter.post("/update/:id",updateBlog);
+blogRouter.post("/:id",getById);
 export default blogRouter;
