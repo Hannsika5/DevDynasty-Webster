0fe8bebe90d5a7def1deb6686e09459354ef6068
kusumag004 <kusumag004@gmail.com>
2023-11-12 12:21:26 +0530

    changes made

--- backend/routes/blog-routes.js
+++ backend/routes/blog-routes.js
@@ -1,7 +1,11 @@
 import express from 'express';
-import {getAllBlogs} from "../controllers/Blog-controller.js";
+import {getAllBlogs,addBlog,updateBlog,getById,deleteBlog,getByUserId} from "../controllers/Blog-controller.js";
 const blogRouter=express.Router();
 
 blogRouter.get("/",getAllBlogs);
-
+blogRouter.post("/add",addBlog);
+blogRouter.put("/update/:id",updateBlog);
+blogRouter.get("/:id",getById);
+blogRouter.delete("/:id",deleteBlog);
+blogRouter.get('/user/:id',getByUserId);
 export default blogRouter;
