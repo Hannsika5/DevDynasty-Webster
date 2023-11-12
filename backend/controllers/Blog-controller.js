213ca0dbd6e131105f9b6b5a2302aa388f5adaf3
Muskan-Yadav-369 <148032461+Muskan-Yadav-369@users.noreply.github.com>
2023-11-12 12:19:07 +0530

    Update Blog-controller.js

--- backend/controllers/Blog-controller.js
+++ backend/controllers/Blog-controller.js
@@ -12,3 +12,62 @@ export const getAllBlogs=async(req,res,next)=>{
     }
     return res.status(200).json({blogs})
 }
+
+
+
+
+export const addBlog=async(req,res,next)=>{
+    const{title,description,image,user}=req.body;
+    const blog=new Blog({
+        title,
+        description,
+        image,
+        user,
+    });
+    try{
+       await blog.save()
+    }catch(err){
+        return console.log(err)
+    }
+    return res.status(200).json({blog})
+    };
+
+
+
+    export const updateBlog=async(req,res,next)=>{
+        const {title,description}=req.body;
+        const blogId=req.params.id;
+        let blog;
+        try{
+          
+             blog=await Blog.findByIdAndUpdate(blogId,{
+                title,
+                description,
+    
+        })
+    }
+    catch(err){
+       return console.log(err)
+        }
+        if(!blog){
+            return res.status(500).json({message:"unable to update"})
+        }
+        return res.status(200).json({blog});
+        };
+
+
+
+        export const getById=async(req,res,next)=>{
+            const id=req.params.id;
+            let blog;
+            try{
+                blog=Blog.findById(id);
+            }catch(err){
+                return console.log(err);
+            }
+            if(!blog){
+                return res.status(200).json({blog});
+            
+            }
+        };
+        
